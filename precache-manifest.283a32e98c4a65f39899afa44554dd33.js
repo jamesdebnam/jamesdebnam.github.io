ac4027094150f0e8b644dc6d36ea30e8458e5ae8
jamesdebnam <jamesdebnam@gmail.com>
2020-06-27 09:24:02 +0100

    Updates

---
+++ precache-manifest.283a32e98c4a65f39899afa44554dd33.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71700a546b74dd6e6cc7ff06f66a376d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23ecc025616034c25cd7",
+    "url": "/static/css/main.d17c3cf3.chunk.css"
+  },
+  {
+    "revision": "4c4366a6b1eec002798b",
+    "url": "/static/js/2.229ff2cc.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.229ff2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ecc025616034c25cd7",
+    "url": "/static/js/main.144a9571.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "2fe0a354eedc99b2adfd45c1d76d776a",
+    "url": "/static/media/book-finder.2fe0a354.png"
+  },
+  {
+    "revision": "b2acfc5542253e534f7fa8c4ec75c113",
+    "url": "/static/media/calculator.b2acfc55.png"
+  },
+  {
+    "revision": "4643ffe048395c127076731bb075809f",
+    "url": "/static/media/exchange.4643ffe0.png"
+  },
+  {
+    "revision": "065f8708bcad651694a19fdf771b4665",
+    "url": "/static/media/metronome.065f8708.png"
+  },
+  {
+    "revision": "aa2bb77596da7966fd3cbb72826b2341",
+    "url": "/static/media/todos.aa2bb775.png"
+  }
+]);
